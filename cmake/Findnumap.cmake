8ba5028c7fe6ff97d6abe625f4f723f68f65ccb0
François Trahay <francois.trahay@telecom-sudparis.eu>
2017-03-07 16:06:12 +0100

    - fix the detection of numap - generate a pkgconfig file

--- cmake/Findnumap.cmake
+++ cmake/Findnumap.cmake
@@ -14,49 +14,9 @@
 #
 
 pkg_search_module(NUMAP REQUIRED numap)
-
-FIND_LIBRARY(NUMAP_LIBRARY numap
-  PATHS
-  /usr/lib
-  /usr/local/lib
-  ${NUMAP_DIR}
-  ${NUMAP_DIR}/lib
-)
-
-IF(NUMAP_LIBRARY)
-  GET_FILENAME_COMPONENT(NUMAP_GUESSED_INCLUDE_DIR_tmp "${NUMAP_LIBRARY}" PATH)
-  STRING(REGEX REPLACE "lib$" "include" NUMAP_GUESSED_INCLUDE_DIR "${NUMAP_GUESSED_INCLUDE_DIR_tmp}")
-ENDIF(NUMAP_LIBRARY)
-
-FIND_PATH( NUMAP_INCLUDE_DIR numap.h
-  PATHS
-  ${NUMAP_GUESSED_INCLUDE_DIR}
-  ${NUMAP_DIR}/include
-  /usr/include
-  /usr/local/include
-)
+STRING(REPLACE ";" " " NUMAP_LDFLAGS "${NUMAP_LDFLAGS}")
+STRING(REPLACE ";" " " NUMAP_CFLAGS "${NUMAP_CFLAGS}")
+STRING(REPLACE ";" " " NUMAP_CFLAGS_OTHER "${NUMAP_CFLAGS_OTHER}")
+STRING(REPLACE ";" " " NUMAP_LDFLAGS_OTHER "${NUMAP_LDFLAGS_OTHER}")
 
 
-IF( NUMAP_INCLUDE_DIR )
-  IF( NUMAP_LIBRARY )
-    SET( NUMAP_FOUND "YES" )
-    MARK_AS_ADVANCED( NUMAP_DIR )
-    MARK_AS_ADVANCED( NUMAP_INCLUDE_DIR )
-    MARK_AS_ADVANCED( NUMAP_LIBRARY )
-  ENDIF( NUMAP_LIBRARY )
-ENDIF( NUMAP_INCLUDE_DIR )
-
-
-
-IF( NOT NUMAP_FOUND )
-  MESSAGE("NUMAP installation was not found. Please provide NUMAP_DIR:")
-  MESSAGE("  - through the GUI when working with ccmake, ")
-  MESSAGE("  - as a command line argument when working with cmake e.g. ")
-  MESSAGE("    cmake .. -DNUMAP_DIR:PATH=/usr/local/numap ")
-  MESSAGE("Note: the following message is triggered by cmake on the first ")
-  MESSAGE("    undefined necessary PATH variable (e.g. NUMAP_INCLUDE_DIR).")
-  MESSAGE("    Providing NUMAP_DIR (as above described) is probably the")
-  MESSAGE("    simplest solution unless you have a really customized/odd")
-  MESSAGE("    NUMAP installation...")
-  SET(NUMAP_DIR "" CACHE PATH "Root of NUMAP install tree." )
-ENDIF( NOT NUMAP_FOUND )
