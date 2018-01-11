aa0fbc33d5d3e9695f26932bf7daccf7bfb871ca
Francois Trahay <francois.trahay@telecom-sudparis.eu>
2018-01-11 16:07:47 +0100

    fix teh python script

--- src/counters_to_binding.py
+++ src/counters_to_binding.py
@@ -5,6 +5,7 @@ input_file=open(sys.argv[1], "r")
 nb_nodes=int(sys.argv[2]);
 name=sys.argv[3];
 buffer_size=sys.argv[4];
+threshold=3;
 
 line_no=0
 # first, read the file and count the number of memory access per numa node
@@ -36,10 +37,15 @@ for line in counters:
     cur_node_counter=max(line);
     cur_node=line.index(cur_node_counter);
 
-    if cur_node_counter != 0 and cur_node != prev_node and cur_node_counter > 1:
+    if cur_node_counter != 0 and cur_node != prev_node and cur_node_counter > threshold:
         if prev_node != -1:
-            blocks[nblocks]["end_page"]=cur_block;
-            blocks[nblocks]["counters"]=block_counters;
+            b=blocks[nblocks];
+            b["end_page"]=cur_block;
+            b["counters"]=block_counters;
+            density=b["counters"]/(b["end_page"]-b["start_page"]);
+            if density <= 1:
+                nblocks=nblocks -1;
+
 
         blocks.append({});
         nblocks=nblocks+1;
@@ -57,7 +63,8 @@ blocks[nblocks]["end_page"]=cur_block;
 blocks[nblocks]["counters"]=block_counters;
 
 print "begin_block";
-print name+" "+buffer_size+" "+str(nblocks);
+print name+" "+buffer_size+" "+str(nblocks+1);
 for b in blocks:
-    print str(b["node"])+" "+str(b["start_page"])+" "+str(b["end_page"])+" "+str(b["counters"]);
+    if b :
+        print str(b["node"])+" "+str(b["start_page"])+" "+str(b["end_page"])+" "+str(b["counters"]);
 print "end_block";
