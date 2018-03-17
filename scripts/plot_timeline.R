189b81e412acca3e16c1018ed2f8496a9314cc3e
francois.trahay <francois.trahay@telecom-sudparis.eu>
2018-03-17 19:20:00 +0100

    typos

--- scripts/plot_timeline.R
+++ scripts/plot_timeline.R
@@ -9,12 +9,12 @@ if(length(args)>1){
   output_file=paste(input_file,".png", sep="")
 }
 
-partx=100
+partx=1
 if(length(args)>2){
   partx=as.numeric(args[3]);
 }
 
-data<-read.table(args[1], sep=" ", colClasses="character")
+data<-read.table(input_file, sep=" ", colClasses="character")
 
 # rename columns
 names(data)<-c("cpu", "timestamp", "addr", "offset")
@@ -26,6 +26,9 @@ data["timestamp"]=lapply(data["timestamp"], function(x) {as.numeric(x);})
 max_offset=range(data["offset"])[2]
 graduations=seq(0, max_offset, 20971520)
 labels=graduations/(2097152/2)
+
+min_ts=range(data["timestamp"])[1]
+data["timestamp"]=lapply(data["timestamp"], function(x) {x-min_ts;})
 
 limx=c(range(data["timestamp"])[1], range(data["timestamp"])[2])
 # extract partx % of the data on the x axis
@@ -39,7 +42,7 @@ require(ggplot2)
 #library(scales)
 
 p1<-qplot(data=data, x=timestamp, y=offset, colour=cpu, size=I(0.5), xlab="timestamp", xlim=limx)+
-		     theme(axis.text.x = element_blank(), legend.position="none") +
+#		     theme(axis.text.x = element_blank(), legend.position="none") +
 		     scale_y_continuous(breaks=graduations, labels=labels)+
 		     geom_hline(yintercept = graduations)
 
