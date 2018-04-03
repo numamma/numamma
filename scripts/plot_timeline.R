ef277f8d9806f5844cef4781cb4f967aa9e326d5
Manuel Selva <manuel.selva@inria.fr>
2018-04-03 11:38:32 +0200

    updated script plotting accesses on timeline

--- scripts/plot_timeline.R
+++ scripts/plot_timeline.R
@@ -1,49 +1,79 @@
 #!/usr/bin/R
 
+# Input file parameter
 args<-commandArgs(TRUE)
 input_file=args[1]
 
+# Read input file, rename columns, change type
+data<-read.table(input_file, sep=" ", colClasses="character")
+names(data)<-c("Thread", "timestamp", "addr", "offset")
+data["offset"]=lapply(data["offset"], function(x) {as.numeric(x);})
+data["timestamp"]=lapply(data["timestamp"], function(x) {as.numeric(x);})
+
+# Output file parameter
 if(length(args)>1){
   output_file=args[2];
 } else {
   output_file=paste(input_file,".png", sep="")
 }
 
-partx=1
+# Horizontal lines parameter
+oneMiB=1024*1024
 if(length(args)>2){
-  partx=as.numeric(args[3]);
+    hlinesP = as.integer(args[3]);
+    hlinesMiB = oneMiB * hlinesP
+    max_offset = range(data["offset"])[2]
+    graduations = seq(0, max_offset, hlinesMiB)
+    customLabels = graduations / oneMiB
+    #customLabels = lapply(customLabels, function(x) {paste(x, " MiB");})
 }
 
-data<-read.table(input_file, sep=" ", colClasses="character")
+# Vertical lines parameter
+oneSec = 1E9
+vlinesP = oneSec
+if(length(args)>3){
+    vlinesP = as.integer(args[4]);
+} 
 
-# rename columns
-names(data)<-c("cpu", "timestamp", "addr", "offset")
+# Part of the trace to plot parameter:
+# 1 is all, 0.5 is first half
+partx=1
+if(length(args)>4){
+  partx=as.numeric(args[5]);
+}
 
-#data["offset"]=lapply(data["offset"], function(x) {as.integer(x);})
-data["offset"]=lapply(data["offset"], function(x) {as.numeric(x);})
-data["timestamp"]=lapply(data["timestamp"], function(x) {as.numeric(x);})
+# Update timestamp values such that it starts at 0
+tsRange = range(data["timestamp"]) 
+minTs = tsRange[1]
+data["timestamp"] = lapply(data["timestamp"], function(x) {x-minTs;})
+tsRange = range(data["timestamp"])
+stopifnot(tsRange[1] == 0)
 
-max_offset=range(data["offset"])[2]
-graduations=seq(0, max_offset, 20971520)
-labels=graduations/(2097152/2)
+# Extract partx % of the data on the x axis
+tsRangeLength = tsRange[2] - tsRange[1]
+newMaxTs = tsRangeLength * partx
+newTsRange = c(tsRange[1], tsRange[1] + newMaxTs)
 
-min_ts=range(data["timestamp"])[1]
-data["timestamp"]=lapply(data["timestamp"], function(x) {x-min_ts;})
+# Set time ticks and ticks labels
+print(newTsRange[2])
+oneSec = 1E9
+timeGraduations = seq(newTsRange[1], newTsRange[2], vlinesP)
+print(timeGraduations)
+customTimeLabels = timeGraduations / oneSec
+print(customTimeLabels)
 
-limx=c(range(data["timestamp"])[1], range(data["timestamp"])[2])
-# extract partx % of the data on the x axis
-xrange=(limx[2]-limx[1])
-xrange=xrange*partx
-limx[2]=limx[1]+xrange
-
-labels=lapply(labels, function(x) {paste(x, " MB");})
-
+# Plot the data and save into file
 require(ggplot2)
-#library(scales)
-
-p1<-qplot(data=data, x=timestamp, y=offset, colour=cpu, size=I(0.5), xlab="timestamp", xlim=limx)+
-		     theme(axis.text.x = element_blank(), legend.position="none") +
-		     scale_y_continuous(breaks=graduations, labels=labels)+
-		     geom_hline(yintercept = graduations)
-
+p1<-qplot(data = data,
+          x = timestamp,
+          y = offset,
+          colour = Thread,
+          size = I(0.5), # size of the dots
+          ylab = "Offset (MiB)",
+          xlab = "Time (second)")#,
+          #xlim = newTsRange)
+p1 = p1 + scale_x_continuous(breaks=timeGraduations, labels=customTimeLabels)
+if (exists("customLabels")) {
+    p1 = p1 + scale_y_continuous(breaks=graduations, labels=customLabels)
+}
 ggsave(output_file, plot = p1)
