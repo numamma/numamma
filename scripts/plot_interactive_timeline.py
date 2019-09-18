b57e7219cac05a18850407a244ba70940da9234f
François Trahay <francois.trahay@telecom-sudparis.eu>
2019-09-18 17:54:48 +0200

    improve the interactive plot  script

--- scripts/plot_interactive_timeline.py
+++ scripts/plot_interactive_timeline.py
@@ -12,12 +12,19 @@ df = pd.read_csv(sys.argv[1], delimiter=' ')
 df['timestamp']=(df['timestamp']-min(df['timestamp']))/1e9
 start_time = min(df['timestamp'])
 end_time = max(df['timestamp'])
+
+df['#thread_rank']=df['#thread_rank'].astype('str')
+
 # set the access_weight to 1 to all the line with access_weight=0
 #df['access_weight']=df.loc[df['access_weight'==0]]=1
-df.loc[df['access_weight'] == 0,"access_weight"]=1
-df.loc[df['access_weight'] > 200,"access_weight"]=200
+df['plot_size'] = df['access_weight']
+df.loc[df['plot_size'] == 0,"plot_size"]=1
+df.loc[df['plot_size'] > 200,"plot_size"]=200
 print("start_time:"+str(start_time));
 print("end_time:"+str(end_time));
 
-fig = px.scatter(df, x='timestamp', y='offset', color='#thread_rank', size='access_weight')
+ymax=max(df['offset']);
+fig = px.scatter(df,  x='timestamp', y='offset', color='#thread_rank', size='plot_size', hover_data=['mem_level','access_weight'], hover_name="mem_level", range_y=[0,ymax])
+
+fig.update_layout(title_text='Memory access to buffer '+sys.argv[1]);
 fig.show()
