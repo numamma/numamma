c7fb82d910b86f9fb6465a42c8e145c8a829710a
Jannis Klinkenberg <j.klinkenberg@itc.rwth-aachen.de>
2022-06-07 09:22:44 +0200

    set path to custom directories

--- install_everything.sh
+++ install_everything.sh
@@ -41,16 +41,16 @@ else
     echo "libbacktrace is already installed"
 fi
 
-#libpfm-4.10.1
-PFM_ROOT=$ROOT_DIR/libpfm-4.10.1
-PFM_INSTALL_ROOT=$ROOT_INSTALL_DIR/libpfm-4.10.1
+#libpfm
+PFM_ROOT=$ROOT_DIR/libpfm
+PFM_INSTALL_ROOT=$ROOT_INSTALL_DIR/libpfm
 
 if ! [ -d $PFM_INSTALL_ROOT ]; then
     echo "Installing libpfm..."
     rm -rf $PFM_ROOT
     git clone https://git.code.sf.net/p/perfmon2/libpfm4 $PFM_ROOT || exit 1
     cd $PFM_ROOT || exit 1
-    git checkout v4.10.1 || exit 1
+    git checkout v4.11.1 || exit 1
     make PREFIX=$PFM_INSTALL_ROOT -j4 install || exit 1
 else
     echo "libpfm is already installed"
@@ -108,7 +108,7 @@ if ! [ -d $NUMAP_INSTALL_ROOT ]; then
     echo "Installing numap..."
     rm -rf $NUMAP_ROOT || exit 1
 
-    git clone https://github.com/numap-library/numap.git $NUMAP_ROOT || exit 1
+    git clone git@github.com:jklinkenberg/numap.git $NUMAP_ROOT || exit 1
     cd $NUMAP_ROOT  || exit 1
     mkdir build || exit 1
     cd build
